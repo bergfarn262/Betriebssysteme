ca869cb74d2dfef6d099311da2ef60ec6c963a86
Michael Busch <b.michael.2002@web.de>
2023-05-22 19:07:04 +0200

    Aufgabe 4 bis nummer 6 funktioniert bis auf fehlerbehandlung

--- A4/main.cpp
+++ A4/main.cpp
@@ -3,9 +3,10 @@
 
 #define RESERVED 'R'
 #define CORRUPTED 'D'
-#define FREE '1'
-#define OCCUPIED '0'
+#define FREE '0'
+#define OCCUPIED '1'
 
+int numberOfBlocksGlobal = 0;
 
 struct BsCluster{
     BsCluster* preElement;
@@ -46,6 +47,16 @@ bool checkConvention(const char* name, const unsigned int length){
     return true;
 }
 
+int getFreeDiskSpace(BsFat* bsFat){
+    int freeCounter = 0;
+    for (int i = 0; i < (bsFat->fatSize / bsFat->blockSize); i++) {
+        if(bsFat->status[i] == FREE){
+            freeCounter++;
+        }
+    }
+    return freeCounter;
+}
+
 void createFile(BsFat* bsFat, int sizeFile, char* filename, unsigned char attribute, int position, int length){
     bsFat->files[position] = new BsFile;
     bsFat->files[position]->name = new char[length];
@@ -56,33 +67,32 @@ void createFile(BsFat* bsFat, int sizeFile, char* filename, unsigned char attrib
         bsFat->files[position]->attributes[0] = attribute;
         bsFat->files[position]->size = sizeFile;
     }
-     int numberOfBlocks = ceil((double)sizeFile / (double) bsFat->blockSize + 1);
+     int numberOfBlocks = ceil((double)sizeFile / (double) bsFat->blockSize);
+    numberOfBlocksGlobal += numberOfBlocks;
 
     BsCluster** bsClusters = new BsCluster*[numberOfBlocks];
 
     for (int i = 0; i < numberOfBlocks; i++) {
         bsClusters[i] = new BsCluster;
     }
+
     bsClusters[0]->preElement = nullptr;
     bsClusters[numberOfBlocks - 1]->nextElement = nullptr;
+    bsFat->files[position]->cluster = bsClusters[0];
 
     if(numberOfBlocks > 2){
         for (int i = 1; i < numberOfBlocks - 1; i++) {
+            bsClusters[i-1]->nextElement = bsClusters[i];
             bsClusters[i]->preElement = bsClusters[i-1];
             bsClusters[i]->nextElement = bsClusters[i+1];
+            bsClusters[i+1]->preElement = bsClusters[i];
         }
 
-        bsFat->files[position]->cluster = bsClusters[0];
         bsClusters[numberOfBlocks - 1]->preElement = bsClusters[numberOfBlocks - 2];
 
-    }else if(numberOfBlocks == 1){
-
-        bsFat->files[position]->cluster = bsClusters[0];
-
-    }else{
-        bsFat->files[position]->cluster = bsClusters[0];
-        bsClusters[0]->nextElement = bsClusters[1];
+    }else if (numberOfBlocks == 2){
         bsClusters[1]->preElement = bsClusters[0];
+        bsClusters[0]->nextElement = bsClusters[1];
     }
 
     unsigned int pos = 0;
@@ -90,11 +100,16 @@ void createFile(BsFat* bsFat, int sizeFile, char* filename, unsigned char attrib
     for (int i = 0; i < numberOfBlocks; i++) {
         pos = (rand() % (bsFat->fatSize / bsFat->blockSize));
         while(bsFat->status[pos] != FREE){
+            if(getFreeDiskSpace(bsFat) == 0){
+                break;
+            }
             pos = (rand() % (bsFat->fatSize / bsFat->blockSize));
         }
         bsClusters[i]->positionInStatusArray = pos;
-        bsFat->status[i] = OCCUPIED;
+        bsFat->status[pos] = OCCUPIED;
+        std::cout << pos << ";"; // nur zur Ausgabe
     }
+    std::cout << std::endl; // nur zur Ausgabe
 }
 
 void createFiles(BsFat* bsFat){
@@ -102,7 +117,7 @@ void createFiles(BsFat* bsFat){
     char** fileNames = new char*[10] {"programm1.c", "2programm.c", "prog3.c.cpp", "p4rogramm1.c", "program5m.c", "pr6ogramm.c", "progra7mm.c", "programm8.c", "progr9amm.c", "program.cpp"};
 
     for (int i = 0; i < 10; i++) {
-        createFile(bsFat, (unsigned int) (rand() % 1700 + 1), fileNames[i], (unsigned char) (rand() % 253 + 1), i, length);
+        createFile(bsFat, (unsigned int) (rand() % 2100 + 1), fileNames[i], (unsigned char) (rand() % 253 + 1), i, length);
     }
 }
 
@@ -123,7 +138,7 @@ BsFat* createBsFat(unsigned int _blockSize, unsigned int _fatSize){
     bsFat->status[10] = CORRUPTED;
     bsFat->status[21] = CORRUPTED;
 
-    for (int i = 0; i < _fatSize / _blockSize; ++i) {
+    for (int i = 0; i < _fatSize / _blockSize; ++i) { // nur zur Ausgabe
         std::cout << bsFat->status[i] << ", ";
     }
 
@@ -131,13 +146,29 @@ BsFat* createBsFat(unsigned int _blockSize, unsigned int _fatSize){
 
     createFiles(bsFat);
 
-    for (int i = 0; i < _fatSize / _blockSize; ++i) {
+    for (int i = 0; i < _fatSize / _blockSize; ++i) { // nur zur Ausgabe
         std::cout << bsFat->status[i] << ", ";
     }
+
+    std::cout << std::endl; // nur zur Ausgabe
+
+    for (int i = 0; i < 10; i++) {
+        BsCluster* bsCluster = bsFat->files[i]->cluster;
+        std::cout << "Groesse File: " << bsFat->files[i]->size << "Mb;  "; // nur zur Ausgabe
+        while(bsCluster != nullptr){
+            std::cout << bsCluster->positionInStatusArray << ", "; // nur zur Ausgabe
+            bsCluster = bsCluster->nextElement;
+        }
+        std::cout << std::endl; // nur zur Ausgabe
+    }
+
+    std::cout << std::endl; // nur zur Ausgabe
 
     return bsFat;
 }
 
 int main() {
     createBsFat(512, 16384);
+
+    std::cout << "Number of Blocks: " <<  numberOfBlocksGlobal << std::endl;
 }
